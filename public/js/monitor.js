f0958a53377c540aebe5f36e418f75339c459a8a
rscoates <richard.coates@bytelondon.com>
2020-11-11 09:27:47 +0000

    Don't re-render on same location.

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -1,34 +1,48 @@
-function init () {
-
+function init() {
   var map = null
   var marker = null
   var setlat = null
+
+  var lastLat = null
+  var lastLng = null
 
   var bbt = new BBT('MNolpo28Q3I1PbVpAozZxY8E', {
     auth_endpoint: '/auth'
   })
 
-  bbt.read({
-    owner: 'rscoates',
-    channel: 'AlexTracker',
-    resource: 'alextracker',
-  }, function(err, msg) {
-    var {latitude, longitude} = JSON.parse(msg[0].data)
-    console.log('received position: ', latitude, longitude)
-    displayCarLocation(latitude, longitude)
-  })
+  bbt.read(
+    {
+      owner: 'rscoates',
+      channel: 'AlexTracker',
+      resource: 'alextracker'
+    },
+    function(err, msg) {
+      var { latitude, longitude } = JSON.parse(msg[0].data)
+      console.log('received position: ', latitude, longitude)
+      displayCarLocation(latitude, longitude)
+    }
+  )
 
-  bbt.subscribe({
-    channel: 'AlexTracker',
-    resource: 'alextracker',
-    read: true,
-    write: false
-  }, function(msg) {
-    console.log('received position: ', msg.data.latitude, msg.data.longitude)
-    displayCarLocation(msg.data.latitude, msg.data.longitude)
-  })
+  bbt.subscribe(
+    {
+      channel: 'AlexTracker',
+      resource: 'alextracker',
+      read: true,
+      write: false
+    },
+    function(msg) {
+      console.log('received position: ', msg.data.latitude, msg.data.longitude)
+      displayCarLocation(msg.data.latitude, msg.data.longitude)
+    }
+  )
 
-  function initializeMap (position) {
+  function round(num, dps) {
+    const divisor = 10 ** dps
+
+    Math.round((num + Number.EPSILON) * divisor) / divisor
+  }
+
+  function initializeMap(position) {
     var options = {
       zoom: 17,
       center: position,
@@ -39,6 +53,15 @@ function init () {
   }
 
   function displayCarLocation(lat, lng) {
+    var roundedLat = round(lat)
+    var roundedLng = round(lng)
+
+    if (lastLat == roundedLat && lastLng == roundedLng) {
+      return
+    }
+
+    lastLat = roundedLat
+    lastLng = roundedLng
 
     if (!map || !setlat) {
       setlat = true
@@ -56,6 +79,6 @@ function init () {
       draggable: false,
       map: map
     })
-    map.setCenter({lat, lng})
+    map.setCenter({ lat, lng })
   }
 }
